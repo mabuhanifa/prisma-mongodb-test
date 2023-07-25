f6b693d6c42b2561493c77f77850b37dae0e381d
shourovahmd <moh.abuhanifa@gmail.com>
2023-07-25 14:52:12 +0600

    prisma models

---
+++ src/utils/connectDB.ts
@@ -0,0 +1,10 @@
+// export async function connectDB() {
+//   try {
+//     await prisma.$connect();
+//     console.log("Connected to the database");
+
+//     // Start your server or perform other operations here
+//   } catch (error) {
+//     console.error("Error connecting to the database:", error);
+//   }
+// }
