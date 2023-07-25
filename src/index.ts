f6ef9e0c3e5bfca0202354f150c4e467e28dfd07
shourovahmd <moh.abuhanifa@gmail.com>
2023-07-25 16:25:04 +0600

    middleware

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
+import cookieParser from "cookie-parser";
 import cors from "cors";
 import * as dotenv from "dotenv";
 import express from "express";
+import { connectDB } from "./utils/connectDB";
 dotenv.config();
 
 const port = process.env.PORT || 3000;
@@ -9,6 +11,15 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+connectDB();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
 app.listen(port, () => {
   console.log(`App is running on port ${port}!`);
 });
